256540dcd0c6ef6dd4726c0724e8fd989f564643
render41 <gabrielservice05@gmail.com>
2024-01-17 17:02:13 -0300

    JSON Response

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>My List Works</h1>')
-    console.log(res.statusCode)
+// Route Test
+app.get("/", (req, res) => {
+  res.send("<h1>My List Works</h1>");
+  console.log(res.statusCode);
+});
+
+// Route JSON
+app.get("/json", (req, res) => {
+  res.json({ title: "Tarefa X", done: true });
 });
 
 app.listen(3000, () => {
-    console.log('Server Start');
+  console.log("Server Start");
 });
