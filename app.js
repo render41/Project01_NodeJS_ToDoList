ea47b9bbcc04b251d003256a609e567e86ff867b
render41 <gabrielservice05@gmail.com>
2024-01-17 16:19:27 -0300

    Start Route

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>My List Works</h1>')
+    console.log(res.statusCode)
+});
+
+app.listen(3000, () => {
+    console.log('Server Start');
+});
